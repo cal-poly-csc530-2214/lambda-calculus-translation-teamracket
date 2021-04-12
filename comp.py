d12e125dd0e8d79f4d45940e6b5161836fef5b09
slin35 <slin35@calpoly.edu>
2021-04-12 14:54:12 -0700

    updated lambda to correct format

--- comp.py
+++ comp.py
@@ -3,7 +3,7 @@ import sexpdata
 from sexpdata import loads, dumps
 
 
-def compiler(exp, local_symbols = []):
+def compiler(exp):
     parsed_exp = loads(exp)
 
     # checks for num
@@ -12,7 +12,6 @@ def compiler(exp, local_symbols = []):
 
     # checks for symbols
     elif type(parsed_exp) is sexpdata.Symbol:
-        local_symbols.append(parsed_exp)
         return dumps(parsed_exp)
         
     # checks plus and multiplication    
@@ -26,30 +25,26 @@ def compiler(exp, local_symbols = []):
         if (dumps(parsed_exp[0]) == 'ifleq0'):
             return "if " + compiler(dumps(parsed_exp[1])) + ":\n\t" + compiler(dumps(parsed_exp[2])) +"\nelse:\n\t"+compiler(dumps(parsed_exp[3]))
         elif dumps(parsed_exp[0]) == '/':
-            res = compiler(dumps(parsed_exp[3]))
-            arguments = ""
-            for symbol in local_symbols:
-                arguments += dumps(symbol)
-            return compiler(dumps(parsed_exp[1])) + " = lambda " + arguments + " : " + res
+            return "lambda " + compiler(dumps(parsed_exp[1])) + " : " + compiler(dumps(parsed_exp[3]))
     # check f the functions are being applied or if there is a print statement
     elif len(parsed_exp) == 2:
         if dumps(parsed_exp[0]) == 'println':
             return "print(\"" + dumps(parsed_exp[1]) + "\")"
         else:
-            res = compiler(dumps(parsed_exp[0]))
-            return"" + res+"\n"+"(" + res[0]+")" + "("+  compiler(dumps(parsed_exp[1]))+")"
+            return "(" + compiler(dumps(parsed_exp[0])) + ")(" + compiler(dumps(parsed_exp[1])) + ")"
+ 
     
            
 
 def main():
-    print(compiler('2', []))
-    print(compiler('a', []))
-    print(compiler('(/ plusone => (+ x 1))', []))
-    print(compiler('(+ 1 2)', []))
-    print(compiler('(* 1 2)', []))
-    print(compiler('(ifleq0 1 2 3)', []))
-    print(compiler('((/ y => (+ x 1)) 2)', []))
-    print(compiler('(println 1)', []))
+    print(compiler('2'))
+    print(compiler('a'))
+    print(compiler('(+ 1 2)'))
+    print(compiler('(* 1 2)'))
+    print(compiler('(ifleq0 1 2 3)'))
+    print(compiler('(println 1)'))
+    print(compiler('(/ x => (+ x 1))'))
+    print(compiler('((/ x => (+ x 1)) 2)'))
     
 
 if __name__ == '__main__':
